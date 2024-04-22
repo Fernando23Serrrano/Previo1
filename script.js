5f9b0806916ebbe23793d79402d7828d475b05ed
Fernando23Serrrano <107650413+Fernando23Serrrano@users.noreply.github.com>
2024-04-22 12:00:50 -0500

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+    event.preventDefault(); // Evitar el envío del formulario por defecto
 
     var codigo = document.getElementById('codigo').value;
     var password = document.getElementById('password').value;
 
-    fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            codigo: codigo,
-            password: password
-        })
-    })
+    // Realizar la solicitud GET a la API para obtener la lista de usuarios
+    fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/students')
     .then(response => {
         if (!response.ok) {
-            throw new Error('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+            throw new Error('Error al obtener la lista de usuarios');
         }
         return response.json();
     })
-    .then(data => {
-        console.log('Inicio de sesión exitoso:', data);
-        alert('Inicio de sesión exitoso. Bienvenido!');
+    .then(usuarios => {
+        // Verificar si el código y la contraseña coinciden con algún usuario
+        var usuarioValido = usuarios.find(usuario => usuario.codigo === codigo && usuario.clave === password);
 
-        mostrarInterfazNotas(codigo);
+        if (usuarioValido) {
+            // Si el usuario es válido, mostrar la interfaz de notas
+            alert('Inicio de sesión exitoso. Bienvenido!');
+            mostrarInterfazNotas(codigo);
+        } else {
+            // Si el usuario no es válido, mostrar un mensaje de error
+            alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+        }
     })
     .catch(error => {
         console.error('Error:', error);
-        alert(error.message);
+        alert('Error al obtener la lista de usuarios. Por favor, inténtalo de nuevo.');
     });
 });
+
+
 function mostrarInterfazNotas(codigo) {
     // Ocultar el formulario de inicio de sesión
     document.getElementById('loginForm').style.display = 'none';
